34f422b86f436dcf7e80b10bbdab47a2413c373f
Leo Schwarz <mail@leoschwarz.com>
2016-11-29 22:33:54 +0100

    Some refactoring and partial implementation of write generator.rb

--- generate.rb
+++ generate.rb
@@ -71,34 +71,58 @@ class Field
     end
 end
 
+class Block
+    attr_accessor :fields, :quantity, :quantity_count
+
+    def initialize(name, quantity, quantity_count, fields, message)
+        @name = name
+        @quantity = quantity
+        @quantity_count = quantity_count
+        @fields = fields
+        @message = message
+    end
+
+    # Rust struct name.
+    def r_name
+        "#{@message.name}_#{@name}"
+    end
+
+    # Field name.
+    def f_name
+        @name.underscore
+    end
+end
+
 # Extractor for blocks.
-def extract_block(lines)
+def extract_block(lines, message)
     blk_name, blk_quantity, blk_quantity_count = lines.shift.split
     blk_fields = []
     lines.each do |line|
         _, field_name, field_type, field_count, _ = line.split
         blk_fields << Field.new(field_name, field_type, field_count.to_i)
     end
-    OpenStruct.new({
-        name: blk_name,
-        quantity: blk_quantity,
-        quantity_count: blk_quantity_count,
-        fields: blk_fields
-    })
+    Block.new(blk_name, blk_quantity, blk_quantity_count, blk_fields, message)
 end
 
 # Extractor for messages.
 # Gets an array of lines that have to be extracted.
 def extract_message(lines)
     # Parse descriptor of message.
-    msg_name, msg_freq, msg_trust, msg_enc = lines.shift.split(" ")
+    msg_name, msg_freq, msg_id, msg_trust, msg_enc = lines.shift.split(" ")
+    message = OpenStruct.new({
+        name: msg_name,
+        frequency: msg_freq,
+        id: msg_id,
+        trust: msg_trust,
+        encoding: msg_enc,
+        blocks: []
+    })
 
     # Remove opening and closing braces of block specifications.
     lines.shift
     lines.pop
 
     # Extract block specifications.
-    msg_blocks = []
     block_raw = nil
     lines.each do |line|
         if block_raw.nil?
@@ -107,21 +131,15 @@ def extract_message(lines)
         elsif line[0] == "{"
             block_raw << line
         else
-            msg_blocks << extract_block(block_raw)
+            message.blocks << extract_block(block_raw, message)
             block_raw = [line]
         end
     end
     unless block_raw.nil?
-        msg_blocks << extract_block(block_raw)
+        message.blocks << extract_block(block_raw, message)
     end
 
-    OpenStruct.new({
-        name: msg_name,
-        frequency: msg_freq,
-        trust: msg_trust,
-        encoding: msg_enc,
-        blocks: msg_blocks
-    })
+    message
 end
 
 # Handle messages independently.
@@ -160,8 +178,7 @@ def generate_struct(message)
     # to avoid name clashes block names are {messagename}_{blockname}
     code = ""
     message.blocks.each do |block|
-        name = "#{message.name}_#{block.name}"
-        code << "pub struct #{name} {\n"
+        code << "pub struct #{block.r_name} {\n"
         block.fields.each do |field|
             code << "\tpub #{field.r_name}: #{field.r_type},\n"
         end
@@ -172,13 +189,12 @@ def generate_struct(message)
     code << "pub struct #{message.name} {\n"
 
     message.blocks.each do |block|
-        block_name = "#{message.name}_#{block.name}"
         if block.quantity.downcase == "single"
-            code << "\tpub #{block.name.underscore}: #{block_name},\n"
+            code << "\tpub #{block.f_name}: #{block.r_name},\n"
         elsif block.quantity.downcase == "multiple"
-            code << "\tpub #{block.name.underscore}: [#{block_name}; #{block.quantity_count}],\n"
+            code << "\tpub #{block.f_name}: [#{block.r_name}; #{block.quantity_count}],\n"
         elsif block.quantity.downcase == "variable"
-            code << "\tpub #{block.name.underscore}: Vec<#{block_name}>,\n"
+            code << "\tpub #{block.f_name}: Vec<#{block.r_name}>,\n"
         end
     end
 
@@ -199,18 +215,81 @@ end
 # Generate writers. #
 #####################
 
+# Returns the byte array for a given message that can be written after the header
+# to indicate the type of the message.
+def generate_message_id_bytes(message)
+    full = message.id.to_s(16).rjust(8, "0")
+    if message.frequency == "High"
+        "[0x#{full[6..7]}]"
+    elsif message.frequency == "Medium"
+        "[0xff, 0x#{full[6..7]}]"
+    elsif message.frequency == "Low"
+        "[0xff, 0xff, 0x#{full[4..5]}, 0x#{full[6..7]}]"
+    else
+        raise "Can't generate message type bytes for message: #{message}"
+    end
+end
+
 # For a given field a single line writing the field once to a writer called `writer`.
 # Since it is possible that we will enumerate over multiple sources in loops, the source
 # object is provided here as an argument (of type string)
 def generate_field_writer(field, source)
-    if %w[u8 u16 u32 u64 i8 i16 i32 i64].include? field.r_type
-        t = field.r_type
-        "try!(buffer.write_#{t}::<LittleEndian>(#{source}.#{field.r_name}));\n"
-    elsif field.r_type == "Uuid"
-        "try!(buffer.write(#{source}.#{field.r_name}.as_bytes()));\n"
+    value = "#{source}.#{field.r_name}"
+    r_type = field.r_type
+    if %w[u8 u16 u32 u64 i8 i16 i32 i64].include? r_type
+        "try!(buffer.write_#{r_type}::<LittleEndian>(#{value}));\n"
+    elsif %w[f32 f64].include? r_type
+        "try!(buffer.write_#{r_type}::<LittleEndian>(#{r_type}));\n"
+    elsif r_type == "Uuid"
+        "try!(buffer.write(#{value}.as_bytes()));\n"
+    elsif r_type == "Ip4Addr"
+        "try!(buffer.write(#{value}.octets()));\n"
+    elsif r_type == "Ip4Port"
+        "try!(buffer.write_u16::<LittleEndian>(#{value}));\n"
+    elsif r_type == "Vector3<f32>" or r_type == "Vector3<f64>"
+        f_type = r_type[8..10]
+        "try!(buffer.write_#{f_type}::<LittleEndian>(#{value}.x));\n" +
+        "try!(buffer.write_#{f_type}::<LittleEndian>(#{value}.y));\n" +
+        "try!(buffer.write_#{f_type}::<LittleEndian>(#{value}.z));\n"
+    elsif r_type == "Vector4<f32>"
+        "try!(buffer.write_f32::<LittleEndian>(#{value}.x));\n" +
+        "try!(buffer.write_f32::<LittleEndian>(#{value}.y));\n" +
+        "try!(buffer.write_f32::<LittleEndian>(#{value}.z));\n" +
+        "try!(buffer.write_f32::<LittleEndian>(#{value}.w));\n"
+    elsif r_type == "Quaternion<f32>"
+        puts "WARNING: Writing Quaternions is not yet implemented."
+        nil
+        # TODO: This one might be a bit more tricky:
+        # can we just use a polar decomposition and discard the norm part?
+        # I don't really know much about Quaternions so postponed until it
+        # becomse a relevant issue.
+    elsif r_type == "bool"
+        "try!(buffer.write_u8(#{value} as u8));\n"
     else
         raise "No rule for field writer generation of field: #{field}"
     end
+end
+
+def generate_message_impl(message)
+    out = ""
+    out << "impl Message for #{message.name} {\n"
+    out << "\tfn write_to<W: Write>(&self, buffer: &mut W) -> WriteMessageResult {\n"
+    out << "\t\t// Write the message number.\n"
+    out << "\t\ttry!(buffer.write(&#{generate_message_id_bytes(message)}));\n"
+
+    message.blocks.each do |block|
+        if block.quantity == "Single"
+            out << "\t\t// Block #{block.r_name}"
+            out << "TODO"
+        else
+            puts "Write implementation for blocks with quantity other than single not yet available."
+            return ""
+        end
+    end
+
+    out << "\tOk(())\n"
+    out << "\t}\n"
+    out
 end
 
 if system 'which rustfmt'
