e12b39694bec28cf5e80a4b703652ba2e9f12f13
Leo Schwarz <mail@leoschwarz.com>
2017-11-19 17:20:49 +0100

    sketch of System trait

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -4,3 +4,31 @@
 
 pub mod agent_update;
 pub mod initiation;
+
+use messages::MessageInstance;
+
+/*
+// TODO: Consider whether for our purposes we want to keep this composable, or just
+//       implement the things we need and avoid the boxing?
+
+/// Trait to enable composable handling of messages received through the circuit.
+pub trait System {
+    /// Called as soon as the connection is established.
+    ///
+    /// This allows a System instance to be passed to a Circuit instance,
+    /// before starting the communication, so that no message will be missed.
+    fn handle_start(&self);
+
+    /// Called after connection is lost, or immediately before breaking the connection.
+    fn handle_stop(&self);
+
+    /// Called when a message is received, and it was not yet eaten by any
+    /// of the System instances with a higher priority.
+    fn handle_message(&self, message: MessageInstance);
+}
+
+struct SystemInstance {
+    instance: Box<System>,
+    priority: f32,
+}
+*/
