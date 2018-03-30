c9780968fe740b800dc40afe0f10de33a065f79b
Leo Schwarz <mail@leoschwarz.com>
2018-03-30 14:50:35 +0200

    Refactor TerrainService Receivers

--- src/services/mod.rs
+++ src/services/mod.rs
@@ -9,8 +9,18 @@ pub trait Service {
     // TODO: This is a rather ugly solution to the problem, ideally this could be
     // passed with the register_service function, however note the
     // complications in the usage in the Simulator initalization.
+    //
+    // → TODO A potential solution could be to pass in a struct, which will be
+    // populated        later on and can be unwraped if needed.
     fn register_message_sender(&mut self, _sender: MessageSender) {}
 }
+
+/*
+pub struct CircuitData {
+    pub region_id: Uuid,
+    pub message_sender: MessageSender,
+}
+*/
 
 pub mod region_handle;
 pub mod terrain;
--- src/services/terrain.rs
+++ src/services/terrain.rs
@@ -8,26 +8,113 @@ use logging::{Log, Logger};
 use messages::{MessageInstance, MessageType};
 use services::Service;
 use std::cell::Cell;
+use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
+use types::Uuid;
 
-// TODO: Implement an iterator method, which deletes all disconnected channels
-// when iterating through this in a safe manner.
+#[derive(Debug, Fail)]
+pub enum ReceiversError {
+    #[fail(display = "Attempted to register Receivers twice for region: {}", 0)]
+    RegisterTwice(Uuid),
+
+    #[fail(display = "Receiver for TerrainService already moved, for region: {}", 0)]
+    ReceiverMoved(Uuid),
+}
+
+/// Manages the receivers of a set of regions.
+///
+/// Receivers will be removed when iterating received patches,
+/// when found to be disconnected.
 pub struct Receivers {
-    pub land_patches: crossbeam_channel::Receiver<Vec<Patch>>,
+    receivers: HashMap<Uuid, Receiver>,
+}
+
+impl Receivers {
+    pub fn new() -> Self {
+        Receivers {
+            receivers: HashMap::new(),
+        }
+    }
+
+    /// Register a terrain service to this receivers manager.
+    ///
+    /// TODO: In the future region_id might be optional if this data can be
+    /// passed       to tReceiversError
+    pub fn register(
+        &mut self,
+        region_id: Uuid,
+        terrain_service: &TerrainService,
+    ) -> Result<(), ReceiversError> {
+        if self.receivers.contains_key(&region_id) {
+            return Err(ReceiversError::RegisterTwice(region_id));
+        }
+
+        if let Some(receiver) = terrain_service.receiver.replace(None) {
+            self.receivers.insert(region_id, receiver);
+            Ok(())
+        } else {
+            Err(ReceiversError::ReceiverMoved(region_id))
+        }
+    }
+
+    /// Iterates through all currently available patches and invokes
+    /// `handler` for each of them.
+    pub fn receive_patches<F: Fn(Patch)>(&mut self, handler: F) {
+        let mut disconnected = Vec::new();
+
+        for (region_id, receiver) in self.receivers.iter_mut() {
+            let lock = receiver.channel_lock.lock().unwrap();
+
+            loop {
+                match receiver.land_patches.try_recv() {
+                    Ok(patches) => {
+                        for patch in patches {
+                            handler(patch);
+                        }
+                    }
+                    Err(crossbeam_channel::TryRecvError::Empty) => {
+                        break;
+                    }
+                    Err(crossbeam_channel::TryRecvError::Disconnected) => {
+                        disconnected.push(region_id.clone());
+                        break;
+                    }
+                }
+            }
+
+            drop(lock);
+        }
+
+        // Remove disconnected receivers.
+        for region_id in disconnected.iter() {
+            self.receivers.remove(region_id);
+        }
+    }
+}
+
+/// Contains the receivers for one region.
+struct Receiver {
+    channel_lock: Arc<Mutex<()>>,
+    land_patches: crossbeam_channel::Receiver<Vec<Patch>>,
 }
 
 pub struct TerrainService {
-    receivers: Cell<Option<Receivers>>,
+    receiver: Cell<Option<Receiver>>,
 }
 
 impl Service for TerrainService {
     fn register_service(handlers: &mut message_handlers::Handlers, log: &Log) -> Self {
+        // TODO: Refactor this code a bit, especially the many cloned Arc are a bit
+        // ugly.
         let (patch_tx, patch_rx) = crossbeam_channel::bounded(100);
         let patch_tx = Arc::new(Mutex::new(patch_tx));
+        let channel_lock = Arc::new(Mutex::new(()));
+        let channel_lock2 = Arc::clone(&channel_lock);
         let logger = Arc::new(Logger::root(log.clone(), o!("service" => "TerrainService")));
 
         let handler = move |msg: MessageInstance, context: &message_handlers::HandlerContext| {
             let patch_tx = Arc::clone(&patch_tx);
+            let channel_lock3 = Arc::clone(&channel_lock2);
             match msg {
                 MessageInstance::LayerData(msg) => {
                     debug!(logger, "Received new layer data msg");
@@ -36,8 +123,11 @@ impl Service for TerrainService {
                     let patches = context.cpupool.spawn_fn(move || {
                         extract_land_patch(&msg).map(|patches| {
                             debug!(logger2, "Decoding layer data ok.");
+                            // Wait for potential reads of the values.
+                            let lock = channel_lock3.lock().unwrap();
                             let tx = patch_tx.lock().unwrap();
                             tx.send(patches).unwrap();
+                            drop(lock);
                         })
                     });
                     let fut = patches.map_err(move |e| {
@@ -58,20 +148,13 @@ impl Service for TerrainService {
         };
         handlers.register_type(MessageType::LayerData, Box::new(handler));
 
-        let receivers = Receivers {
+        let receiver = Receiver {
+            channel_lock,
             land_patches: patch_rx,
         };
 
         TerrainService {
-            receivers: Cell::new(Some(receivers)),
+            receiver: Cell::new(Some(receiver)),
         }
-    }
-}
-
-impl TerrainService {
-    /// Returns the Receivers on the first invocation, afterwards only None
-    /// will be returned.
-    pub fn receivers(&self) -> Option<Receivers> {
-        self.receivers.replace(None)
     }
 }
